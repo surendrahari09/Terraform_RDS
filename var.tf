5111b9325699b1ee5b5df23e1329bfea77f2e10b
root <root@ip-172-31-0-6.ap-south-1.compute.internal>
2022-10-26 10:37:25 +0000

    modified

---
+++ var.tf
@@ -0,0 +1,10 @@
+variable "engine" {}
+variable "engine_version" {}     
+variable "instance_class" {}
+variable "db_name"  {}       
+variable "username" {}  
+variable "password" {} 
+variable "parameter_group_name" {}
+variable "db_subnet_group_name" {}
+variable "publicly_accessible" {}
+variable "skip_final_snapshot" {}
