dbe3755488a1cf018bae0e0e5d8934bfc3f24170
surendrahari09 <115442378+surendrahari09@users.noreply.github.com>
2022-11-01 16:55:40 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,13 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-    source  = "hashicorp/aws"
-    version = "~> 4.1"
-    }
-  }
 
-  required_version = ">= 0.14.9"
-}
 
 provider "aws" {
   region     = "ap-south-1"
